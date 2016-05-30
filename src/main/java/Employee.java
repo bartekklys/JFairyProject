dea08b3ba7c5664d850b4fe0954bf9f22bfca3bd
bartek <klys.bartosz@gmail.com>
2016-05-30 16:33:06 +0200

    created abstract class employee

---
+++ src/main/java/Employee.java
@@ -0,0 +1,47 @@
+public abstract class Employee {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private int nationalIdentyficationNumber;
+
+    public Employee(String firstName, String lastName, int age, int nationalIdentyficationNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.nationalIdentyficationNumber = nationalIdentyficationNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getNationalIdentyficationNumber() {
+        return nationalIdentyficationNumber;
+    }
+
+    public void setNationalIdentyficationNumber(int nationalIdentyficationNumber) {
+        this.nationalIdentyficationNumber = nationalIdentyficationNumber;
+    }
+}
